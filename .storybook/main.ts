0179ac292f6be023b2eccbb83a6049004ad0d9cf
Ashish Chaudhary <147826671+Ash1022004@users.noreply.github.com>
2025-08-18 18:03:44 +0530

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,19 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
-  framework: { name: "@storybook/react-vite", options: {} },
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"  // ✅ must include tsx
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    name: "@storybook/react-vite",   // if using Vite
+    options: {}
+  }
 };
 export default config;
+
